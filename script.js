5cbf8d5f9bba9087e27c6fb2736714b1289bb99b
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2024-08-25 16:43:19 +0200

    Update the result display
    The result is now displayed in a much more convenient way

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const userInput = document.querySelector('#text-input');
 const result = document.querySelector('#result');
+const userWord = document.querySelector('.user-word');
 const checkButton = document.querySelector('#check-btn');
 
 
@@ -17,11 +18,16 @@ const userInputReversed = userInputFiltered.split('').reverse().join('');
 console.log(userInputReversed); 
 
 if (userInputFiltered.toLowerCase() === userInputReversed.toLowerCase()) {
-    result.textContent = `${userInput.value} is a palindrome.`
-    result.style.color = 'green';
+  console.log('U kurac ')
+    userWord.textContent = `${userInput.value}`;
+    result.textContent = `is a palindrome.`;
+    userWord.style.color = 'green';
+    result.style.color = 'green'; 
 } else {
-    result.textContent = `${userInput.value} is not a palindrome.`
-    result.style.color = 'red';
+  userWord.textContent = `${userInput.value} `;
+  result.textContent = ` is not a palindrome.`;
+  userWord.style.color = 'red';
+  result.style.color = 'red';
 }
 });
 
